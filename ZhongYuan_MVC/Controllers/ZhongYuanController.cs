340a889b93303718dc012192633527bbbe7b5de8
1223861671@qq.com <1223861671@qq.com>
2019-03-14 11:29:59 +0800

    添加项目文件。

---
+++ ZhongYuan_MVC/Controllers/ZhongYuanController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZhongYuan_MVC.Controllers
+{
+    public class ZhongYuanController : Controller
+    {
+        // GET: ZhongYuan
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
